101e0a246a61f8af1f27636c5b8a9a5f3cffff19
asli <aslicvd09@gmail.com>
2026-08-23 23:07:39 -0600

    matame dios

--- conexion.php
+++ conexion.php
@@ -1,14 +1,15 @@
 <?php
+
 $host     = getenv('DB_HOST')     ?: 'mysql-143672f9-aslicvd09-f409.i.aivencloud.com';
 $user     = getenv('DB_USER')     ?: 'avnadmin';
-$password = getenv('DB_PASS')     ?: 'AVNS_PuVvsawjq4xpduyda42';
+$password = getenv('DB_PASS');    
 $database = getenv('DB_NAME')     ?: 'defaultdb';
-$port     = getenv('DB_PORT')     ?: 27037;
+$port     = (int)(getenv('DB_PORT') ?: 27037);
 
 $conex = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conex) {
-    die("Error de conexión: " . mysqli_connect_error());
+    die("Error de conexión a la base de datos: " . mysqli_connect_error());
 }
 
 mysqli_set_charset($conex, "utf8mb4");
