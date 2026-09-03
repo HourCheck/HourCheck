c28a813833e14e846e879b5bb134ad7ad15fbc30
asli <aslicvd09@gmail.com>
2026-09-02 23:37:59 -0600

    cebada

--- firestorerules.js
+++ firestorerules.js
@@ -2,8 +2,19 @@ rules_version = '2'; // Tells Firebase to use the latest rules logic
 service cloud.firestore {
   match /databases/{database}/documents {
     
-    // This is where you write your specific collection rules!
+    // Regla para la colección de usuarios
     match /users/{userId} {
+      allow read, write: if request.auth != null;
+    }
+
+    // Permite que CUALQUIERA lea las instituciones (necesario para el formulario)
+    match /Institucion/{document} {
+      allow read: if true;
+      allow write: if request.auth != null;
+    }
+
+    // Permite el registro y lectura en la colección de Estudiantes
+    match /Estudiantes/{studentId} {
       allow read, write: if request.auth != null;
     }
 
